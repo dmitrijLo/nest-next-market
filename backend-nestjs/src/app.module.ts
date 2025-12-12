d3c45f4832410678152aa36ebb92ba6e1fa398b4
dmitrijLo <demilorenz@googlemail.com>
2025-12-12 15:55:06 +0100

    add dependencies for swagger, class-validator, class-transformer and wire up everything

--- backend-nestjs/src/app.module.ts
+++ backend-nestjs/src/app.module.ts
@@ -2,8 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Product } from './products/entities/product.entity';
+import { ProductsModule } from './products/products.module';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configSrv: ConfigService) => ({
+        type: 'postgres',
+        url: configSrv.getOrThrow<string>('DATABASE_URL'),
+        entities: [Product],
+        synchronize: configSrv.get('NODE_ENV') !== 'production', // ONLY FOR DEVELOPMENT!!!
+      }),
+    }),
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend-nestjs/src/main.ts
+++ backend-nestjs/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Minimarket API')
+    .setDescription('API for Fullstack App Minimarket')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
