0d951b8a93644a9716a64e6371f4fc6ec190c7c4
dmitrijLo <demilorenz@googlemail.com>
2025-12-16 12:46:05 +0100

    add get base route utility function

---
+++ frontend-nextjs/utils/api.ts
@@ -0,0 +1,11 @@
+export function getBaseApiUrl() {
+  if (typeof window !== "undefined") {
+    return process.env.NEXT_PUBLIC_API_URL;
+  }
+
+  if (process.env.INTERNAL_API_URL) {
+    return process.env.INTERNAL_API_URL;
+  }
+
+  return process.env.NEXT_PUBLIC_API_URL;
+}
